24d39d35df29a122d799e71a948c2f94926eeb4d
Lars Wobus <lars.wobus@web.de>
2018-12-01 23:26:53 +0100

    Add C# docs for Curve.cs

--- CurveEditor/Assets/EssentialPackages/CurveEditor/Scripts/Editor/Curve.cs
+++ CurveEditor/Assets/EssentialPackages/CurveEditor/Scripts/Editor/Curve.cs
@@ -3,13 +3,30 @@ using UnityEngine;
 
 namespace EssentialPackages.CurveEditor.Editor
 {
+	/// <summary>
+	/// Container to bring name and animation curve together.
+	/// </summary>
 	[Serializable]
 	public class Curve
 	{
+		/// <summary>
+		/// Custom name for the animation curve.
+		/// </summary>
 		[SerializeField] private string _name;
+		
+		/// <summary>
+		/// Two-dimensional animation curve.
+		/// </summary>
 		[SerializeField] private AnimationCurve _animationCurve;
 		
+		/// <summary>
+		/// Get name of the stored animation curve.
+		/// </summary>
 		public string Name => _name;
+		
+		/// <summary>
+		/// Get animation curve.
+		/// </summary>
 		public AnimationCurve AnimationCurve => _animationCurve;
 	}
 }
