e09ed4ca1a02ee4b68dc432a3dce04af814252ab
Lars Wobus <lars.wobus@web.de>
2018-12-01 15:00:58 +0100

    Curves can be distributed between projects

---
+++ CurveEditor/Assets/EssentialPackages/CurveEditor/Scripts/Editor/Curve.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+
+namespace EssentialPackages.CurveEditor.Editor
+{
+	[Serializable]
+	public class Curve
+	{
+		[SerializeField] private string _name;
+		[SerializeField] private AnimationCurve _animationCurve;
+		
+		public string Name => _name;
+		public AnimationCurve AnimationCurve => _animationCurve;
+	}
+}
---
+++ CurveEditor/Assets/EssentialPackages/CurveEditor/Scripts/Editor/CurveList.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace EssentialPackages.CurveEditor.Editor
+{
+	[CreateAssetMenu(fileName = "CurveList", menuName = "Essential/CurveEditor/CurveList", order = 1)]
+	public class CurveList : ScriptableObject
+	{
+		[SerializeField] private Curve[] _curve;
+	}
+}
