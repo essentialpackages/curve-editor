acb23cf41732a1d3d2d28870273bc39a0ac98011
Lars Wobus <lars.wobus@web.de>
2018-12-01 17:00:57 +0100

    Add C# doc for Inspector.cs

--- CurveEditor/Assets/EssentialPackages/CurveEditor/Scripts/Editor/Utils/Inspector.cs
+++ CurveEditor/Assets/EssentialPackages/CurveEditor/Scripts/Editor/Utils/Inspector.cs
@@ -3,12 +3,32 @@ using UnityEngine;
 
 namespace EssentialPackages.CurveEditor.Editor.Utils
 {
+	/// <summary>
+	/// Contains 
+	/// </summary>
 	public static class Inspector
 	{
+		/// <summary>
+		/// Get the width of the GUI area for the current EditorWindow or other view.
+		/// </summary>
 		private static float CurrentViewWidth => EditorGUIUtility.currentViewWidth;
+		
+		/// <summary>
+		/// Get the width in pixels reserved for labels of Editor GUI controls.
+		/// </summary>
 		private static float LabelWidth => EditorGUIUtility.labelWidth;
+		
+		/// <summary>
+		/// Get the height used for a single Editor control such as a one-line EditorGUI.TextField or EditorGUI.Popup.
+		/// </summary>
 		private static float SingleLineHeight => EditorGUIUtility.singleLineHeight;
 	
+		/// <summary>
+		/// Create property field including a label.
+		/// </summary>
+		/// <param name="rect">Rect of a single element from a reorderable list.</param>
+		/// <param name="offsetY">Specifies the height inside the editor window.</param>
+		/// <param name="property">Serialized property from any element inside a reorderable list.</param>
 		public static void CreatePropertyField(Rect rect, float offsetY, SerializedProperty property)
 		{
 			EditorGUI.LabelField(
