0e0f206ee8173aa79f611d3ca37329492962f32c
Lars Wobus <lars.wobus@web.de>
2018-12-01 23:10:30 +0100

    Add C# docs for CurveList.cs

--- CurveEditor/Assets/EssentialPackages/CurveEditor/Scripts/Editor/CurveList.cs
+++ CurveEditor/Assets/EssentialPackages/CurveEditor/Scripts/Editor/CurveList.cs
@@ -2,9 +2,16 @@
 
 namespace EssentialPackages.CurveEditor.Editor
 {
+	/// <inheritdoc />
+	/// <summary>
+	/// Redistributable object which holds animation curves.
+	/// </summary>
 	[CreateAssetMenu(fileName = "CurveList", menuName = "Essential/CurveEditor/CurveList", order = 1)]
 	public class CurveList : ScriptableObject
 	{
+		/// <summary>
+		/// Array of labeled animation curves.
+		/// </summary>
 		[SerializeField] private Curve[] _curve;
 	}
 }
